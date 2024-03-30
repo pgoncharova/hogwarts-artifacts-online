e8e0a2a1f9b9db2c805d033279a546d9ce59719b
pgoncharova <113141984+pgoncharova@users.noreply.github.com>
2024-03-30 17:48:09 -0500

    User CRUD done! Close #4

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/UserRepository.java
@@ -0,0 +1,7 @@
+package edu.tcu.cs.hogwartsartifactsonline.hogwartsuser;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<HogwartsUser, Integer> {
+
+}
